d17871124af8dddef3fea8b4da9e742d7650e24c
Vitaly Yagufarov <for.viy@gmail.com>
2020-05-27 01:06:43 +0500

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,7 +5,48 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class SqlRuParse {
+    private final static String[] MONTHS = {"янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"};
+
+    public static Timestamp getDate(String date) {
+        String[] arr = date.split(" ");
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = new GregorianCalendar();
+        if (arr[0].equals("сегодня,")) {
+            sb.append(sdf.format(calendar.getTime())).append(" ")
+                    .append(arr[1]).append(":00");
+
+        } else if (arr[0].equals("вчера,")) {
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+            sb.append(sdf.format(calendar.getTime())).append(" ")
+                    .append(arr[1]).append(":00");
+
+        } else {
+            sb.append("20").append(arr[2], 0, 2).append("-")
+                    .append(convertMonth(arr[1])).append("-")
+                    .append(arr[0]).append(" ")
+                    .append(arr[3]).append(":00");
+        }
+        return Timestamp.valueOf(sb.toString());
+    }
+
+    private static String convertMonth(String month) {
+        int result = 0;
+        for (int i = 0; i < MONTHS.length; i++) {
+            if (MONTHS[i].equals(month)) {
+                result = i + 1;
+                break;
+            }
+        }
+        return Integer.toString(result);
+    }
+
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
