c0f58f26ab4cfc70ec072817c5f50e965cce8ff1
Vitaly Yagufarov <for.viy@gmail.com>
2020-05-28 21:00:15 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -48,14 +48,16 @@ public class SqlRuParse {
     }
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element element : row) {
-            if (!element.text().startsWith("Важно:") && !element.child(1).hasClass("closedTopic")) {
-                Element date = element.parent().select(".altCol").last();
-                System.out.println(element.child(0).attr("href"));
-                System.out.println(element.child(0).text());
-                System.out.println(date.text());
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element element : row) {
+                if (!element.text().startsWith("Важно:") && !element.child(1).hasClass("closedTopic")) {
+                    Element date = element.parent().select(".altCol").last();
+                    System.out.println(element.child(0).attr("href"));
+                    System.out.println(element.child(0).text());
+                    System.out.println(date.text());
+                }
             }
         }
     }
